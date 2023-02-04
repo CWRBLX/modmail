5055eb32996729475e1f3a903b5040ffd5f4a5f6
bchurton <ben@churton.uk>
2023-02-04 21:25:05 +0000

    Create closeprompt.js

---
+++ commands/closeprompt.js
@@ -0,0 +1,30 @@
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+
+module.exports = {
+    name: "closeprompt",
+    aliases: ["prompt", "close-prompt"],
+    description: "Ask the user if they want to close the thread",
+    async execute(client, message, args) {
+        try {
+            let ticket;
+            if (message?.guild) ticket = await client.db.collection("modmailThread").findOne({ channel: message.channel.id, closed: false })
+            else ticket = await client.db.collection("modmailThread").findOne({ user: message.author.id, closed: false })
+            if (!ticket) return await message.reply({ content: `:warning: This is not a ModMail thread!` })
+
+            const embed = new MessageEmbed()
+                .setTitle(`Thread Answered`)
+                .setDescription(`Hey ${message.author}, your thread has been marked as answered.\n\n\If that is everything, please use **${client.prefix}close**.`)
+                .setColor(`GREEN`)
+
+            const user = client.users.cache.get(ticket.user) || await client.users.fetch(ticket.user)
+            let sent;
+            await user.send({ embeds: [embed] }).catch(() => sent = false)
+            if (!sent) return await message.reply({ content: `:warning: I was unable to send a message to the user!` })
+
+            await message.reply({ content: `:white_check_mark: Successfully sent the close prompt to the user!` })
+        } catch (err) {
+            console.log(err)
+            await message.reply({ content: client.error(`Error: ${err}`) })
+        }
+    }
+}
